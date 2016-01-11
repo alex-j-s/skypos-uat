6c3b35d01cb3d6a7625e106ef76cc822f139b473
Aaron <aaron@appirio.com>
2016-01-10 16:08:37 -0800

    removed events, added anon ordering

--- static/components/skypos-start/start-controller.js
+++ static/components/skypos-start/start-controller.js
@@ -55,13 +55,16 @@ angular.module('skyZoneApp')
 
             function getGuestId(order){
                 if(!order || !order.orderAccount){
-                    logErrorHideLoading('Sorry, there\'s a problem with that order.');
+                    return -1;
                 }
                 if(order.orderAccount.customer){
                     return order.orderAccount.customer.id;
                 }
-                else{
+                else if(order.orderAccount.group){
                     return order.orderAccount.group.id;
+                }
+                else{
+                    return -1;
                 }
             }
             console.log('++++ GUEST SEARCH RESULT: ', result);
@@ -82,7 +85,13 @@ angular.module('skyZoneApp')
 
             if(result){
                 console.log(result)
-                $scope.goToJumpersScreen(getGuestId(result), result.id);
+                if(getGuestId(result) !== -1){
+                    $scope.goToJumpersScreen(getGuestId(result), result.id);
+                }
+                else{
+                    $location.path('/skypos/offers/'+Park.parkUrlSegment+'/'+result.id);
+
+                }
             }else{
                 logErrorHideLoading('Unable to locate order. Please try again.');
             }
