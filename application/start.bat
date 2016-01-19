f9065e91141516989cac438478849711c7946f1e
Blaine Rothrock <blaine@appirio.com>
2016-01-18 20:12:38 -0600

    start.bat updates

--- application/start.bat
+++ application/start.bat
@@ -1,3 +1,4 @@
-git pull http://SkyPOS@SkyZone123!k@gitlab.com/skyzone/skypos-flask.git dev
+git pull http://SkyPOS:SkyZone123@gitlab.com/skyzone/skypos-flask.git dev
+pause
 start env\Scripts\python server.py
 start chrome http://localhost:5000 --kiosk --disable-cache
---
+++ application/update.bat
@@ -0,0 +1,2 @@
+git pull http://SkyPOS:SkyZone123@gitlab.com/skyzone/skypos-flask.git dev
+pause
