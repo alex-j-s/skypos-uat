a7bc6a329d14d02ebdd3ca9f605550251ada74d5
Aaron <agilbert@appirio.com>
2016-01-18 17:10:46 -0800

    moved start, created shortcut to change icon on

--- application/start.bat
+++ application/start.bat
@@ -1,3 +1,3 @@
 git pull http://SkyPOS@SkyZone123!k@gitlab.com/skyzone/skypos-flask.git
-start application\env\Scripts\python application\server.py
+start env\Scripts\python server.py
 start chrome http://localhost:5000 --kiosk --disable-cache
