d0fedb658c3d48bbe5ee1314127a9869ce871c01
Blaine Rothrock <blaine@appirio.com>
2016-01-18 20:04:53 -0600

    start.bat

--- application/start.bat
+++ application/start.bat
@@ -1,3 +1,3 @@
-git pull http://SkyPOS@SkyZone123!k@gitlab.com/skyzone/skypos-flask.git
+git pull http://SkyPOS@SkyZone123!k@gitlab.com/skyzone/skypos-flask.git dev
 start env\Scripts\python server.py
 start chrome http://localhost:5000 --kiosk --disable-cache
