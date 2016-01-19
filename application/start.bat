392b685ae596de48de1b4bce3752ba2aef19fcfe
Blaine Rothrock <blainerothrock@Blaines-MacBook-Pro-2.local>
2016-01-19 13:40:13 -0800

    removed pull from start.bat

--- application/start.bat
+++ application/start.bat
@@ -1,4 +1,3 @@
-git pull http://SkyPOS:SkyZone123@gitlab.com/skyzone/skypos-flask.git dev
 TaskKill /F /IM chrome.exe
 start env\Scripts\python server.py
 start chrome http://localhost:5000 --kiosk --disable-cache
