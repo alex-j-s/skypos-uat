361dbcc9bc29f0fcbf8c11d06d44a12850f41acf
Blaine Rothrock <blainerothrock@Blaines-MacBook-Pro-2.local>
2016-01-21 11:20:50 -0800

    minor waiver fix + spelling error fix in hardware-confg panel

--- application/static/components/common/directives/jumperrow/jumperrow.js
+++ application/static/components/common/directives/jumperrow/jumperrow.js
@@ -180,9 +180,9 @@ angular.module('skyZoneApp')
                                 if(!$scope.$$phase) {
                                 $scope.$apply();
                                 }
-                                return;
                             };
                         }, 2000);
+                        if (!data.success) { return; }
                         var agreement = {
                             'primarySignature': "TODO: SIGNATURE"
                         };
