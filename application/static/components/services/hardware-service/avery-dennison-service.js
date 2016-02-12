4f23190a5c5c572800f89ee115bb1ad8e0246226
alexjspears <aspears@appirio.com>
2016-02-12 09:29:39 -0500

    avery dennison printer service

--- application/static/components/services/hardware-service/avery-dennison-service.js
+++ application/static/components/services/hardware-service/avery-dennison-service.js
@@ -26,13 +26,21 @@ angular.module('skyZoneApp')
 
 		console.log('hello from avery test print');
 		
-		var command = "{F,25,A,R,E,200,200,\"FMT-25\" | \
-		C,140,40,0,1,2,1,W,C,0,0,\"SAMPLE FORMAT\",0 | \
-		B,1,12,F,85,40,1,2,40,5,L,0 | \
-		T,2,18,V,50,50,1,1,1,1,B,L,0,0,1 | } \
-		{B,25,N,1 | \
-		1,\"02802811111\" | \
-		2,\"TEXT FIELD\" | }"
+		var command = "{F,1,A,R,E,1000,409,\"FMT-25\" | \
+        C,415,305,0,16,3,2,O,L,0,3,\"Sky Zone Sacramento\",0 | \
+        C,355,260,0,1,3,2,O,L,0,3,\"Tester, J\",0 | \
+        C,400,205,0,1,5,4,O,L,0,3,\"530-\",0 | \
+        C,255,205,0,1,5,4,O,L,0,3,\"630\",0 | \
+        C,415,190,0,2,3,2,O,L,0,3,\"Product Name\",0 | \
+        C,415,170,0,2,3,2,O,L,0,3,\"Adolescent\",0 | \
+        C,415,150,0,2,3,2,O,L,0,3,\"Thur. Feb, 11 2015 \",0 | \
+        C,455,125,0,2,3,2,O,L,0,3,\"Here's a line where marketing can say whatever \",0 | \
+        B,1,12,F,85,100,1,2,40,5,L,0 | \
+        T,2,18,V,50,50,1,1,1,1,B,L,0,0,1 | } \
+        {B,1,N,1 | \
+        2,\"\" | }"
+
+  
 	
     	HardwareService.socket.emit('usb-write', { connectionId: self.connectionId, command:command });
     }
