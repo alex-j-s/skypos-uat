c2ca421d5393759790b21595c0fada1e6b19ac2a
Blaine Rothrock <blaine@appirio.com>
2016-02-12 09:25:49 -0600

    udpated my triPOS dev tokens

--- application/static/components/common/access-manager/triPOS-access-manager.js
+++ application/static/components/common/access-manager/triPOS-access-manager.js
@@ -7,16 +7,16 @@ angular.module('skyZoneApp')
 	})
 	.constant('TRIPOS_DEV_TOKENS', {
 		//Blaine's/////////
-		//'DEV-KEY-1':'1ea5d25f-e334-44f7-aea3-f4e180a9bff2',
-		//'DEV-KEY-2':'16b6148c-e19e-4f4a-9f31-27f93e535c9c',
-		//'DEV-SECRET-1':'31d58d53-9968-4ee9-9ca5-6ce420256129',
-		//'DEV-SECRET-2':'ac131ecb-b7d8-4a36-b8be-8614f0bd0d8b',
+		'DEV-KEY-1':'326074b5-870d-4d20-b367-cdbfc2f34eda',
+		'DEV-KEY-2':'e70a65ff-64a1-4b61-aff8-efce19c8e5b',
+		'DEV-SECRET-1':'31d58d53-9968-4ee9-9ca5-6ce420256129',
+		'DEV-SECRET-2':'ac131ecb-b7d8-4a36-b8be-8614f0bd0d8b',
 		
 		/////VIKASH'S/////////
-		 'DEV-KEY-1':'a700df16-a0f9-4ea4-8422-ac7260ca02c1',
-		 'DEV-KEY-2':'2d877d80-f07d-4618-826a-5e3eb5f2a68b',
-		 'DEV-SECRET-1':'0ac9099e-eebd-4ad4-84db-d06f6326a2ba',
-		 'DEV-SECRET-2':'ac027d6f-2756-4fbd-9b06-76d5992ac27a'
+		 // 'DEV-KEY-1':'a700df16-a0f9-4ea4-8422-ac7260ca02c1',
+		 // 'DEV-KEY-2':'2d877d80-f07d-4618-826a-5e3eb5f2a68b',
+		 // 'DEV-SECRET-1':'0ac9099e-eebd-4ad4-84db-d06f6326a2ba',
+		 // 'DEV-SECRET-2':'ac027d6f-2756-4fbd-9b06-76d5992ac27a'
 			
 	})
 	.constant('TRIPOS_HEADERS', {
@@ -126,9 +126,9 @@ angular.module('skyZoneApp')
 						tpAuthHeader += 'Nonce=' + nonce + ', ';
 						tpAuthHeader += 'RequestDate=' + timestamp + ', ';
 						tpAuthHeader += 'Signature=' + signature;
-						config.headers['tp-authorization'] = tpAuthHeader;
+						//config.headers['tp-authorization'] = tpAuthHeader;
 						console.log('tp-authorization: ', config.headers['tp-authorization']);
-						//config.headers['tp-authorization'] = 'Version=1.0, Credential=1ea5d25f-e334-44f7-aea3-f4e180a9bff2'; //Blaine's Machine
+						config.headers['tp-authorization'] = 'Version=1.0, Credential=' + TRIPOS_DEV_TOKENS[DEV-KEY-1]; //Blaine's Machine
 						//config.headers['tp-authorization'] = 'Version=1.0, Credential=a700df16-a0f9-4ea4-8422-ac7260ca02c1';  //Vikash's Machine
 
 						config.headers['tp-request-id'] = HmacService.generateUUID();
