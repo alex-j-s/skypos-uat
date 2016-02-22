4ebc213d213b064c6dcc07d4055c085dffe28ab4
Blaine Rothrock <blaine@appirio.com>
2016-02-22 01:22:27 -0600

    updates to guest search via sky band. code complete for 1.1.1

--- application/static/components/skypos-guest/guest-controller.js
+++ application/static/components/skypos-guest/guest-controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('skyZoneApp')
-    .controller('SPGuestController',['$scope', '$modal', '$routeParams', '$q', '$rootScope', '$location', 'Park', 'ProfileService', 'OrderService', 'NavService', 'Order', 'UserService', 'RFIDReaderService'
+    .controller('SPGuestController',['$scope', '$modal', '$routeParams', '$q', '$rootScope', '$location', 'Park', 'ProfileService', 'OrderService', 'NavService', 'Order', 'UserService', 'RFIDReaderService',
     	function($scope, $modal, $routeParams, $q, $rootScope ,$location, Park, ProfileService, OrderService, NavService, Order, UserService, RFIDReaderService){
 
     	console.log(Park);
@@ -199,11 +199,17 @@ angular.module('skyZoneApp')
 
             skybandModal.result.then( function (skybandId) {
                 $rootScope.$broadcast('szeShowLoading');
-                ProfileService.customerSearch({'skybandId':id})
+                ProfileService.customerSearch({'skybandId':skybandId})
                   .then(function(result) {
                     console.log(result);
+                    if ( result.data.length === 1 ) {
+                        $scope.handleGuestSearchResult(result.data[0]);
+                    } else {
+                        $rootScope.broadcast('szeError','Could not find guest');
+                    }
                   }, function(err) {
                     console.log(err);
+                    $rootScope.broadcast('szeError','error searching guest: ', err.msg);
                   });
             }, function(reason){
 
