4ba77525d98b40ffd9f5f662ad70f565aedf91eb
Aaron <aaron@appirio.com>
2016-02-22 23:36:03 -0800

    fixed redundant act types showing in ticket screen

--- application/static/components/resolves/ActivitiesResolve.js
+++ application/static/components/resolves/ActivitiesResolve.js
@@ -1,8 +1,16 @@
 function ActivitiesResolve(CatalogService, $q, $route) {
     var def = $q.defer();
+    var out = [];
+    var addedTypes = [];
     CatalogService.getActivities($route.current.params.parkUrlSegment).then(function(activities) {
     	console.log(activities)
-        def.resolve(activities);
+    	angular.forEach(activities, function(activity){
+    		if(addedTypes.indexOf(activity.activityType) === -1){
+    			out.push(activity);
+    			addedTypes.push(activity.activityType)
+    		}
+    	})
+        def.resolve(out);
 	}, function(err) {
         def.reject(err)
     });
