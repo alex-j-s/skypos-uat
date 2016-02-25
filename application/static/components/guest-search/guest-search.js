e7fee0b46afaf0b704bcdc8a19ea11e1c76c4e60
Blaine Rothrock <blainerothrock@Blaines-MacBook-Pro-2.local>
2016-02-25 00:44:53 -0800

    guest search modal no longer searches automatically when open with no criteria.

--- application/static/components/guest-search/guest-search.js
+++ application/static/components/guest-search/guest-search.js
@@ -9,9 +9,9 @@
  */
 
 angular.module('skyZoneApp')
-    .controller('GuestSearchCtrl', ['$scope', '$modalInstance', '$modal', '$rootScope', 'ProfileService', 'Criteria', 
+    .controller('GuestSearchCtrl', ['$scope', '$modalInstance', '$modal', '$rootScope', 'ProfileService', 'Criteria',
         function($scope, $modalInstance, $modal, $rootScope, ProfileService, Criteria) {
-      
+
         $scope.isSearching = true;
         $scope.showProfile = false;
       $scope.criteria = Criteria;
@@ -110,9 +110,11 @@ angular.module('skyZoneApp')
             searchCriteria.postalCode.length > 0 ||
             searchCriteria.orderNumber.length > 0 ||
             isPhone(searchCriteria.numOrEmail) ||
-            isEmail(searchCriteria.numOrEmail)); 
+            isEmail(searchCriteria.numOrEmail));
         };
 
-        $scope.guestSearch($scope.criteria);
+        if ( $scope.criteria.phone || $scope.criteria.email ) {
+          $scope.guestSearch($scope.criteria);
+        }
     }
     ]);
